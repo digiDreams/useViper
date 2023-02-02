959f63df60431de6b20611c3743ff668ba15c051
digiDreams <digital2dreamer@gmail.com>
2023-02-02 15:05:36 +0100

    Initial Commit

---
+++ useViper/useViperApp.swift
@@ -0,0 +1,17 @@
+//
+//  useViperApp.swift
+//  useViper
+//
+//  Created by Apprenant 16 on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct useViperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
